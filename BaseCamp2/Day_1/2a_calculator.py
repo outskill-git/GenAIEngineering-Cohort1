88ce307f85777c3b9e7b031734305892e53a515b
RaamRaam <rm.ramanathan@gmail.com>
2025-04-19 20:14:19 +0530

    fixed type casting

--- BaseCamp2/Day_1/2a_calculator.py
+++ BaseCamp2/Day_1/2a_calculator.py
@@ -8,13 +8,13 @@ app = FastAPI()
 @app.get("/add")
 def add(a, b):
     """Add two numbers and return the result."""
-    result = a + b
+    result = int(a) + int(b)
     return {"operation": "add", "a": a, "b": b, "result": result}
 
 @app.get("/subtract")
 def subtract(a, b):
     """Subtract b from a and return the result."""
-    result = a - b
+    result = int(a) - int(b)
     return {"operation": "subtract", "a": a, "b": b, "result": result}
 
 @app.get("/")
